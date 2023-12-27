b0a5031929d795741a3a901ddb2b6c70bab555e5
tk1rk <taylor.kirk@pm.me>
2023-12-27 04:09:15 +0000

    Delete t.sh

--- t.sh
+++
@@ -1,6 +0,0 @@
-docker run -p 8080:8080 \
-  -v "/srv/coder:/home/coder/project" \
-  -u "$(id -u):$(id -g)" \
-  -e "DOCKER_USER=$USER" \
-  -e "PASSWORD=c0d3r" \
-  -it bencdr/code-server-deploy-container:latest
