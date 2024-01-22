fd5e143408c03233ca30c0cceff1b505f40794d4
Taylor Kirk <taylor.kirk@pm.me>
2024-01-22 11:56:28 +0000

    Add .config/zsh/config/bindkeys.zsh

---
+++ .config/zsh/config/bindkeys.zsh
@@ -0,0 +1,25 @@
+# Bindkeys
+#
+
+autoload up-line-or-beginning-search down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+
+bindkey -v
+[[ -n "$terminfo[kpp]"   ]] && bindkey "$terminfo[kpp]"   up-line-or-beginning-search   # PAGE UP
+[[ -n "$terminfo[knp]"   ]] && bindkey "$terminfo[knp]"   down-line-or-beginning-search # PAGE DOWN
+[[ -n "$terminfo[khome]" ]] && bindkey "$terminfo[khome]" beginning-of-line             # HOME
+[[ -n "$terminfo[kend]"  ]] && bindkey "$terminfo[kend]"  end-of-line                   # END
+[[ -n "$terminfo[kdch1]" ]] && bindkey "$terminfo[kdch1]" delete-char                   # DELETE
+[[ -n "$terminfo[kbs]"   ]] && bindkey "$terminfo[kbs]"   backward-delete-char          # BACKSPACE
+
+zflai-assert "${+terminfo[kpp]}${+terminfo[knp]}${+terminfo[khome]}${+terminfo[kend]}" "1111" "terminfo test" "[zshrc] "
+
+bindkey "^A"      beginning-of-line     "^E"      end-of-line
+bindkey "^?"      backward-delete-char  "^H"      backward-delete-char
+bindkey "^W"      backward-kill-word    "\e[1~"   beginning-of-line
+bindkey "\e[7~"   beginning-of-line     "\e[H"    beginning-of-line
+bindkey "\e[4~"   end-of-line           "\e[8~"   end-of-line
+bindkey "\e[F"    end-of-line           "\e[3~"   delete-char
+bindkey "^J"      accept-line           "^M"      accept-line
+bindkey "^T"      accept-line           "^R"      history-incremental-search-backward
