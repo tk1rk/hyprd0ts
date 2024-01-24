619ddf5f571f0127b0e2c338b762bead1547b938
Taylor K <taylor.kirk@pm.me>
2024-01-24 01:24:37 -0800

    Update aliases.zsh

--- .config/zsh/config/aliases.zsh
+++ .config/zsh/config/aliases.zsh
@@ -5,4 +5,15 @@ elif (( $+commands[wget] )); then
   alias get='wget --continue --progress=bar --timestamping'
 fi
 
-alias ls='eza'
+alias ls='eza -lA --header --icons=always --color=always --color-scale --color-scale-mode=gradient --group-directories-first --no-time --git --git-repos'
+
+### CAT & LESS
+command -v bat > /dev/null && \
+        alias bat='bat --theme=ansi' && \
+        alias cat='bat --pager=never' && \
+        alias less='bat'
+# in debian the command is batcat
+command -v batcat > /dev/null && \
+        alias batcat='batcat --theme=ansi' && \
+        alias cat='batcat --pager=never' && \
+        alias less='batcat'
