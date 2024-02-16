e609a2c032eb08835d68e3ac058ef4535d62bb4e
Taylor K <49350410+tk1rk@users.noreply.github.com>
2024-02-15 18:04:24 -0800

    Create h.sh

---
+++ h.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+yay -S hyprland-bin kitty waybar-hyprland \
+    swaybg swaylock-effects wofi wlogout mako thunar \
+    ttf-jetbrains-mono-nerd noto-fonts-emoji \
+    polkit-gnome python-requests starship \
+    swappy grim slurp pamixer brightnessctl gvfs \
+    bluez bluez-utils lxappearance xfce4-settings \
+    dracula-gtk-theme dracula-icons-git xdg-desktop-portal-hyprland-git
