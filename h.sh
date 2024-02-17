4d8dcf7618288592c5781ce5e4c70026f5eac0d5
Taylor K <49350410+tk1rk@users.noreply.github.com>
2024-02-17 15:58:19 -0800

    Delete h.sh

--- h.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-yay -S hyprland-bin kitty waybar-hyprland \
-    swaybg swaylock-effects wofi wlogout mako thunar \
-    ttf-jetbrains-mono-nerd noto-fonts-emoji \
-    polkit-gnome python-requests starship \
-    swappy grim slurp pamixer brightnessctl gvfs \
-    bluez bluez-utils lxappearance xfce4-settings \
-    dracula-gtk-theme dracula-icons-git xdg-desktop-portal-hyprland-git
