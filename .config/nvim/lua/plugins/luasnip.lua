e23ea98d7b283cd651fed6c34af7bc3213017f5f
Taylor K <49350410+tk1rk@users.noreply.github.com>
2024-04-13 00:00:08 -0700

    Update luasnip.lua

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -1,4 +1,4 @@
-{
+return {
   "L3MON4D3/LuaSnip",
   build = (not jit.os:find("Windows"))
       and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp"
