8214b661624b5b8da19a5f30205ddcdc71b88358
Taylor K <49350410+tk1rk@users.noreply.github.com>
2024-05-09 21:08:44 -0700

    Update mtp-device.sh

--- .config/waybar/scripts/mtp-device.sh
+++ .config/waybar/scripts/mtp-device.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export DISPLAY=:0
-export XAUTHORITY=/home/shved/.Xauthority
+export XAUTHORITY=/home/tk/.Xauthority
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
 
 source $HOME/.local/bin/environment.sh
