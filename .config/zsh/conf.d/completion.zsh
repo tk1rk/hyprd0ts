eac3ee7f261ff0d4fa2a35ef1a7d67b6b1d26716
Taylor K. <49350410+tk1rk@users.noreply.github.com>
2024-08-18 19:32:37 -0700

    Delete the file at '.config/zsh/conf.d/completion.zsh'

--- .config/zsh/conf.d/completion.zsh
+++
@@ -1,9 +0,0 @@
-# zsh-autocomplete
-zstyle '*:compinit' arguments -D -i -u -C -w
-# all Tab widgets
-zstyle ':autocomplete:*complete*:*' insert-unambiguous yes
-# all history widgets
-zstyle ':autocomplete:*history*:*' insert-unambiguous yes
-# ^S
-zstyle ':autocomplete:menu-search:*' insert-unambiguous yes
-zstyle ':completion:*:*' matcher-list 'm:{[:lower:]-}={[:upper:]_}' '+r:|[.]=**'
